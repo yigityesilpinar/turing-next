44128783ea44f16229c916bdcd4c55a77722fa8c
Yigit Yesilpinar <Yigit.Yeslipinar@grupawp.pl>
2019-06-25 14:05:01 +0200

    Added styles for SSR

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
 import { PORTAL_ROOT } from '@config/global';
 
-export default class extends Document {
+export default class extends Document<{ styleTags?: string }> {
+    static getInitialProps({ renderPage }: any) {
+        const sheet = new ServerStyleSheet();
+        //  Retrieve styles from components in the page
+        // @ts-ignore
+        const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+
+        // Extract the styles as <style> tags
+        const styleTags = sheet.getStyleElement();
+
+        //  Pass styleTags as a prop
+        return { ...page, styleTags };
+    }
+
     render() {
         return (
             <html>
                 <Head>
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+                    {/* put the styles in the head  */}
+                    {this.props.styleTags}
                     <script src="https://js.stripe.com/v3/" />
                 </Head>
                 <body>
